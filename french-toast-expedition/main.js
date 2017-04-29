c2e597ff8571804b2fb38ead669d2bf6bf33ba6c
Seth Thompson <seth.h.thompson@gmail.com>
2017-04-29 08:09:33 -0400

    Completed exercise

---
+++ french-toast-expedition/main.js
@@ -0,0 +1,10 @@
+// your code, here
+
+let bread = 1.99;
+let milk = 3.89;
+let eggs = 2.29;
+let total = bread + milk + eggs;
+let severeTotal = total * 3;
+
+console.log(total);
+console.log(severeTotal);
